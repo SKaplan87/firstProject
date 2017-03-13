07ec6940904962e651a10c8c39fb8d008f8d8274
Scott Kaplan <scottkaplan.sk@gmail.com>
2017-03-13 11:14:44 -0400

    3/13 commit

--- project1/scripts/scripts.js
+++ project1/scripts/scripts.js
@@ -4,31 +4,19 @@ jQuery(function() {
 let body = $('body');
 let goalie = $('<div id="goalie"></div>');
 let ball = $('<div id="ball"></div>');
-let zone1 = $('<div id="zone1" class="zone">ZONE 1</div>')
-let zone2 = $('<div id="zone2" class="zone">ZONE 2</div>')
-let zone3 = $('<div id="zone3" class="zone">ZONE 3</div>')
+let zone1 = $('<div id="zone1" class="zone"></div>')
+let zone2 = $('<div id="zone2" class="zone"></div>')
+let zone3 = $('<div id="zone3" class="zone"></div>')
+let scoreboard = $('<div id="scoreboard"></div>');
 body.append(goalie);
 body.append(ball);
 body.append(zone1);
 body.append(zone2);
 body.append(zone3);
+body.append(scoreboard);
 let homeScore = 0;
 let awayScore = 0;
 let turn =1;
-
-// let scoreboard = $('<container id="scoreboard></container');
-// let home1 =$('div class="round", id="home1"></div>');
-// let home2 =$('div class="round", id="home2"></div>');
-// let home3 =$('div class="round", id="home3"></div>');
-// let home4 =$('div class="round", id="home4"></div>');
-// let home5 =$('div class="round", id="home5"></div>');
-// let homeTot=$('div class="round", id="homeTot"></div>');
-// let away1 =$('div class="round", id="away1"></div>');
-// let away2 =$('div class="round", id="away2"></div>');
-// let away3 =$('div class="round", id="away3"></div>');
-// let away4 =$('div class="round", id="away4"></div>');
-// let away5 =$('div class="round", id="away5"></div>');
-// let awayTot =$('div class="round", id="awayTot"></div>');
 
 
 //declares offense and defense values to be assigned later
@@ -62,7 +50,6 @@ zone3.on("click",function(){
 function defense()
 {
 let dive=Math.floor(Math.random()*3)+1; //goalie chooses where to dive
-console.log("dive to "+dive) //will delete, for testing
 
   if (dive===1)
   {
@@ -83,11 +70,10 @@ console.log("dive to "+dive) //will delete, for testing
 function oVd(shot,turn)
 {
   dive=defense(); //defense function, returns defense value
-  console.log("dive to "+dive);
   if(shot!==dive)
   {
     console.log("Goal!");
-    if(turn%2=1)
+    if(turn%2===1)
     {
        awayScore++;
     }
@@ -100,14 +86,40 @@ function oVd(shot,turn)
   {
     console.log("save!");
   }
-  setTimeout(reset,2000); //resets board
-  alert(awayScore+" - "+homeScore);
+
+  setTimeout(fieldReset,1500); //resets board
+  let round = Math.ceil(turn/2);
+  scoreboard.text("Round: "+round+" Away:"+awayScore+" - Home:"+homeScore);
+
+  if (turn===10)
+  {
+    if (homeScore>awayScore)
+    {
+      alert("The home team wins!")
+    }
+    else if (awayScore>homeScore)
+    {
+      alert("The away team wins!")
+    }
+    else
+    {
+      alert("Its a draw!")
+    }
+    scoreReset();
+  }
+
 }
 
-function reset()
+function fieldReset()
 {
   goalie.attr('id','goalie');
   ball.attr('id','ball');
+}
+
+function scoreReset()
+{
+  homeScore=0;
+  awayScore=0;
 }
 
 
