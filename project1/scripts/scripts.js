b96f4e72c4128e6029821f9517e047694896f91b
Scott Kaplan <scottkaplan.sk@gmail.com>
2017-03-15 09:47:18 -0400

    new shooting functionality

--- project1/scripts/scripts.js
+++ project1/scripts/scripts.js
@@ -1,22 +1,36 @@
 jQuery(function() {
 
-//Creates field
+//Creates board
 let body = $('body');
 let goalie = $('<div id="goalie"></div>');
 let ball = $('<div id="ball"></div>');
-let zone1 = $('<div id="zone1" class="zone"></div>')
-let zone2 = $('<div id="zone2" class="zone"></div>')
-let zone3 = $('<div id="zone3" class="zone"></div>')
+let zone1 = $('<div id="zone1" class="zone"></div>');
+let zone2 = $('<div id="zone2" class="zone"></div>');
+let zone3 = $('<div id="zone3" class="zone"></div>');
 let scoreboard = $('<div id="scoreboard"></div>');
+let homePoints = $('<div id="homepoints"></div>');
+let awayPoints = $('<div id="awaypoints"></div>');
+let roundNum = $('<div id="roundnum"></div>');
+let notify = $('<div id="notify"></div>');
+let message = $('<p id="message"></p>');
+notify.append(message);
 body.append(goalie);
 body.append(ball);
 body.append(zone1);
 body.append(zone2);
 body.append(zone3);
 body.append(scoreboard);
+scoreboard.append(homePoints);
+scoreboard.append(awayPoints);
+scoreboard.append(roundNum);
+homePoints.text("0");
+awayPoints.text("0");
+roundNum.text("1");
 let homeScore = 0;
 let awayScore = 0;
 let turn =1;
+
+
 
 
 //declares offense and defense values to be assigned later
@@ -81,17 +95,24 @@ function oVd(shot,turn)
     {
       homeScore++;
     }
+    message.text("GOOOOOOOAL");
+    body.append(notify);
+
   }
   else
   {
     console.log("save!");
+    body.append(notify);
+    message.text("SAVE! SAVE! SAVE! SAVE!")
   }
 
   setTimeout(fieldReset,1500); //resets board
   let round = Math.ceil(turn/2);
-  scoreboard.text("Round: "+round+" Away:"+awayScore+" - Home:"+homeScore);
+  homePoints.text(homeScore);
+  awayPoints.text(awayScore);
+  roundNum.text(round);
 
-  if (turn===10)
+  if (turn===10) //tests if game should end
   {
     if (homeScore>awayScore)
     {
@@ -114,12 +135,14 @@ function fieldReset()
 {
   goalie.attr('id','goalie');
   ball.attr('id','ball');
+  notify.remove();
 }
 
 function scoreReset()
 {
   homeScore=0;
   awayScore=0;
+  turn=0;
 }
 
 
